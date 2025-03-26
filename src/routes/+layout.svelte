64f44c5f8d816d1411b9b3161e8d8a1973707457
Hugos68 <hugokorteapple@gmail.com>
2025-03-26 23:29:17 +0100

    oauth working noice

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 import "../app.css";
 
-const { children } = $props();
+const { children, data } = $props();
 </script>
+
+<h1>Hello {data.session ? data.session.user.email : "Guest"}</h1>
 
 {@render children()}
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,2 +1,15 @@
+import { supabase } from "$lib/supabase";
+import { error } from "@sveltejs/kit";
+
+export async function load() {
+    const session_response = await supabase.auth.getSession();
+    if (session_response.error) {
+        error(500, session_response.error.message);
+    }
+    return {
+        session: session_response.data.session
+    }
+}
+
 export const prerender = true;
 export const ssr = false;
