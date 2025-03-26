6806438e64c0192229b5a6f2bb4106880397e8ac
Hugos68 <hugokorteapple@gmail.com>
2025-03-26 23:57:47 +0100

    stufff

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,29 @@
 <script lang="ts">
+import { invalidateAll } from "$app/navigation";
+import { supabase } from "$lib/supabase";
 import "../app.css";
 
 const { children, data } = $props();
+
+$effect(() => {
+	const auth_state_listener = supabase.auth.onAuthStateChange(invalidateAll);
+	return auth_state_listener.data.subscription.unsubscribe();
+});
+
+async function sign_out() {
+	await supabase.auth.signOut();
+}
 </script>
 
-<h1>Hello {data.session ? data.session.user.email : "Guest"}</h1>
+<header class="preset-filled-surface-100-900 p-4 rounded-b-md">
+	<nav class="flex justify-between">
+		<a class="btn preset-filled" href="/">Home</a>
+		{#if data.session}
+			<button class="btn preset-filled" onclick={sign_out}>Sign Out</button>
+		{:else}
+			<a class="btn preset-filled" href="/sign-in">Sign In</a>
+		{/if}
+	</nav>
+</header>
 
 {@render children()}
--- src/routes/sign-in/+page.svelte
+++ src/routes/sign-in/+page.svelte
@@ -1,39 +1,40 @@
 <script lang="ts">
-    import { supabase } from "$lib/supabase";
-    import { onOpenUrl } from "@tauri-apps/plugin-deep-link";
-    import { openUrl } from '@tauri-apps/plugin-opener';
-    import type { Provider } from "@supabase/supabase-js";
-  
-    const providers: Provider[] = ['google'];
-    
-    async function login_with_provider(provider: Provider) {
-      const oauth_response = await supabase.auth.signInWithOAuth({
-        provider: provider,
-        options: {
-          skipBrowserRedirect: true,
-          redirectTo: 'dbd-trial-mapper://url',
-        }
-      });
-      if (oauth_response.error) {
-        throw new Error(oauth_response.error.message);
-      }
-      await openUrl(oauth_response.data.url);
-      await onOpenUrl(async (urls) => {
-        const url = urls.at(0);
-        if (!url) {
-          throw new Error('No URL found');
-        }
-        const code = new URL(url).searchParams.get('code');
-        if (!code) {
-          throw new Error('No code found');
-        }
-        const exchange_response = await supabase.auth.exchangeCodeForSession(code);
-        if (exchange_response.error) {
-          throw new Error(exchange_response.error.message);
-        }
-      });
-    }
-  </script>
+import { supabase } from "$lib/supabase";
+import type { Provider } from "@supabase/supabase-js";
+import { Window } from "@tauri-apps/api/window";
+import { onOpenUrl } from "@tauri-apps/plugin-deep-link";
+import { openUrl } from "@tauri-apps/plugin-opener";
+
+const providers: Provider[] = ["google"];
+
+async function login_with_provider(provider: Provider) {
+	const oauth_response = await supabase.auth.signInWithOAuth({
+		provider: provider,
+		options: {
+			skipBrowserRedirect: true,
+		},
+	});
+	if (oauth_response.error) {
+		throw new Error(oauth_response.error.message);
+	}
+	await openUrl(oauth_response.data.url);
+	await onOpenUrl(async (urls) => {
+		const url = urls.at(0);
+		if (!url) {
+			throw new Error("No URL found");
+		}
+		const code = new URL(url).searchParams.get("code");
+		if (!code) {
+			throw new Error("No code found");
+		}
+		const exchange_response = await supabase.auth.exchangeCodeForSession(code);
+		if (exchange_response.error) {
+			throw new Error(exchange_response.error.message);
+		}
+		await Window.getCurrent().setFocus();
+	});
+}
+</script>
   
   {#each providers as provider}
     <button class="btn preset-filled capitalize" onclick={() => login_with_provider(provider)}>{provider}</button>
