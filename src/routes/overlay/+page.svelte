4aa26c53b5931c6e06667f30bf0070f213496fb6
Hugos68 <hugokorteapple@gmail.com>
2025-03-27 12:04:33 +0100

    stuff

--- src/routes/overlay/+page.svelte
+++ src/routes/overlay/+page.svelte
@@ -1,15 +1,25 @@
 <script lang="ts">
-import { invalidate, invalidateAll } from "$app/navigation";
+import { invalidateAll } from "$app/navigation";
+import { PhysicalSize } from "@tauri-apps/api/dpi";
 import { WebviewWindow } from "@tauri-apps/api/webviewWindow";
 import { Position, moveWindow } from "@tauri-apps/plugin-positioner";
+import { ElementRect } from "runed";
 
 const { data } = $props();
 
-moveWindow(Position.TopRight);
+const overlay = WebviewWindow.getCurrent();
+const documentRect = new ElementRect(() => document.documentElement);
 
-WebviewWindow.getCurrent().listen("update", async () => {
-	await invalidateAll();
+$inspect(documentRect.current.height);
+
+$effect(() => {
+	overlay.setSize(
+		new PhysicalSize(400, Math.round(documentRect.current.height)),
+	);
+	moveWindow(Position.TopRight);
 });
+
+overlay.listen("update", invalidateAll);
 </script>
 
 {#if data.lobby.trial}
