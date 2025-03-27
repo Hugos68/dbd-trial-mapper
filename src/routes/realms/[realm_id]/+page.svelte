128ba2857fa0f730b6cc68fb8450b02a75ffed0d
Hugos68 <hugokorteapple@gmail.com>
2025-03-27 10:29:25 +0100

    stuff

---
+++ src/routes/realms/[realm_id]/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+const { data } = $props();
+
+async function select_trial(trial: (typeof data.trials)[number]) {
+	localStorage.setItem("trial", JSON.stringify(trial));
+}
+</script>
+
+<ol class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+    {#each data.trials as trial (trial.id)}
+        <li>
+            <button onclick={() => select_trial(trial)} class="btn preset-filled w-full aspect-square">{trial.name}</button>
+        </li>
+    {/each}
+</ol>
---
+++ src/routes/realms/[realm_id]/+page.ts
@@ -0,0 +1,15 @@
+import { supabase } from "$lib/supabase/client";
+import { error } from "@sveltejs/kit";
+
+export async function load(event) {
+	const trials_response = await supabase
+		.from("trial")
+		.select("*")
+		.eq("realm", event.params.realm_id);
+	if (trials_response.error) {
+		error(500, trials_response.error.message);
+	}
+	return {
+		trials: trials_response.data,
+	};
+}
