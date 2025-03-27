b50672c2f4d0fd16b17e802dff1370b61bcdd9f1
Hugos68 <hugokorteapple@gmail.com>
2025-03-27 09:53:53 +0100

    Added tooltips

---
+++ src/lib/components/tooltip.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { Tooltip } from "bits-ui";
+	import type {  Snippet } from "svelte";
+ 
+	interface Props extends Tooltip.RootProps {
+		trigger: Required<Tooltip.TriggerProps>['child'];
+        content: Snippet;
+	};
+ 
+	const {
+        trigger,
+        content,
+        ...props
+    }: Props = $props();
+</script>
+ 
+<Tooltip.Root>
+	<Tooltip.Trigger {...props}>
+        {#snippet child(arg)}
+            {@render trigger(arg)}
+        {/snippet}
+	</Tooltip.Trigger>
+	<Tooltip.Portal>
+		<Tooltip.Content class="p-2 preset-filled rounded-md">
+			<Tooltip.Arrow />
+			{@render content?.()}
+		</Tooltip.Content>
+	</Tooltip.Portal>
+</Tooltip.Root>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,8 @@ import { invalidateAll } from "$app/navigation";
 import { supabase } from "$lib/supabase";
 import { Home, LogOut, LogIn, Plus, Users } from '@lucide/svelte'; 
 import "../app.css";
+import { Tooltip as BitsToolTip } from "bits-ui";
+import Tooltip from "$lib/components/tooltip.svelte";
 
 const { children, data } = $props();
 
@@ -12,23 +14,60 @@ async function sign_out() {
 }
 </script>
 
-<header class="preset-filled-surface-100-900 p-4 rounded-b-md flex justify-between">
-	<nav>
-		<a class="btn preset-filled" href="/"><Home /></a>
-	</nav>
-	<nav class="flex gap-2">
-		{#if data.session}
-			<a class="btn preset-filled" href="/lobby/create"><Plus /></a>
-			<a class="btn preset-filled" href="/lobby/join"><Users /></a>
-			<button class="btn preset-filled-error-500" onclick={sign_out}><LogOut /></button>
-		{:else}
-			<a class="btn preset-filled-success-500" href="/sign-in"><LogIn /></a>
-		{/if}
-	</nav>
-</header>
-
-<main class="p-4">
-	{@render children()}
-</main>
+<BitsToolTip.Provider>
+	<header class="preset-filled-surface-100-900 p-4 rounded-b-md flex justify-between">
+		<nav>
+			<Tooltip>
+				{#snippet trigger({ props })}
+					<a {...props} class="btn preset-filled" href="/"><Home /></a>
+				{/snippet}
+				{#snippet content()}
+					<span>Home</span>
+				{/snippet}
+			</Tooltip>
+		</nav>
+		<nav class="flex gap-2">
+			{#if data.session}
+				<Tooltip>
+					{#snippet trigger({ props })}
+						<a {...props} class="btn preset-filled" href="/lobby/create"><Plus /></a>
+					{/snippet}
+					{#snippet content()}
+						<span>Create Lobby</span>
+					{/snippet}
+				</Tooltip>
+				<Tooltip>
+					{#snippet trigger({ props })}
+						<a {...props} class="btn preset-filled" href="/lobby/join"><Users /></a>
+					{/snippet}
+					{#snippet content()}
+						<span>Join Lobby</span>
+					{/snippet}
+				</Tooltip>
+				<Tooltip>
+					{#snippet trigger({ props })}
+						<button {...props} class="btn preset-filled-error-500" onclick={sign_out}><LogOut /></button>
+					{/snippet}
+					{#snippet content()}
+						<span>Sign Out</span>
+					{/snippet}
+				</Tooltip>
+			{:else}
+				<Tooltip>
+					{#snippet trigger({ props })}
+						<a {...props} class="btn preset-filled-success-500" href="/sign-in"><LogIn /></a>
+					{/snippet}
+					{#snippet content()}
+						<span>Sign Out</span>
+					{/snippet}
+				</Tooltip>
+			{/if}
+		</nav>
+	</header>
+	
+	<main class="p-4">
+		{@render children()}
+	</main>
+</BitsToolTip.Provider>
 
 
