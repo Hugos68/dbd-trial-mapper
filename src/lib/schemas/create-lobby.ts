dfe4cf43a9a1bce8fca7cc39fd01725a4fe6a811
Hugos68 <hugokorteapple@gmail.com>
2025-03-27 19:30:19 +0100

    stuff

--- src/lib/schemas/create-lobby.ts
+++
@@ -1,5 +0,0 @@
-import * as v from "valibot";
-
-export const create_lobby_schema = v.object({
-	access: v.picklist(["public", "private"]),
-});
--- src/routes/(authenticated)/+page.svelte
+++
@@ -1,4 +0,0 @@
-<div class="grid place-items-center">
-    <p class="text-surface-500">Join or create a lobby in the top left</p>
-</div>
-
