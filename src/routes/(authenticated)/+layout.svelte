c35bcb633fd80a4cb865709da409e6c426630dc9
Hugos68 <hugokorteapple@gmail.com>
2025-03-28 16:52:52 +0100

    flexible layout

---
+++ src/routes/(authenticated)/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import type { LayoutData } from './$types';
+
+    interface LayoutProps {
+        data: LayoutData
+        title?: string;
+        description?: string;
+    }
+
+    const { children, data } = $props();
+</script>
+
+{@render children()}
