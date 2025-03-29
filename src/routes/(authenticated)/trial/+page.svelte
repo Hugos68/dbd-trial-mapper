988d42158fa34e4923dca79f62e8d6012b08a0b9
Hugos68 <hugokorteapple@gmail.com>
2025-03-29 02:34:30 +0100

    still few bugs here and there

--- src/routes/(authenticated)/trial/+page.svelte
+++ src/routes/(authenticated)/trial/+page.svelte
@@ -1,42 +1,24 @@
 <script lang="ts">
 import { useRealtimeLobby } from "$lib/hooks/use-realtime-lobby.svelte";
-import { XIcon } from "@lucide/svelte";
 
 const { data } = $props();
 
 const lobby = useRealtimeLobby(data.lobby);
-
-async function promisifyUrl(url: string) {
-	const response = await fetch(url);
-	if (!response.ok) {
-		throw new Error(response.statusText);
-	}
-	return URL.createObjectURL(await response.blob());
-}
 </script>
 
-{#if lobby.current.trial}
-    {#await promisifyUrl(lobby.current.trial.image_url)}
-        <div class="grid place-items-center py-16">
-            <span>Loading...</span>
-            <span class="text-sm text-surface-500">Loading trial image</span> 
-        </div>
-    {:then imageUrl}
-        <img class="min-h-full" src={lobby.current.trial?.image_url} alt={lobby.current.trial?.name} />
-    {:catch error}
-        <div class="grid place-items-center py-16">
-            <span>Error loading image</span>
-            <span class="text-sm text-surface-500">{error.message}</span> 
-        </div>
-    {/await}
-{:else}
-    <div class="grid place-items-center py-16">
-        <span>No trial selected</span>
-        <span class="text-sm text-surface-500">
-            Waiting for the host to select a trial
-        </span> 
+<div class="group">
+    <div class="bg-black/0 group-hover:bg-black/50 transition-[background-color] absolute inset-0 grid place-items-center">
+        <a href="/" class="hidden group-hover:block btn preset-filled-primary-500">Hide Trial</a>
     </div>
-{/if}
-<a href="/" class="absolute top-4 right-4"><XIcon /></a>
-
+    {#if lobby.current.trial}
+        <img src={lobby.current.trial.image_url} alt={lobby.current.trial?.name} />
+    {:else}
+        <div class="grid place-items-center py-32">
+            <span>No trial selected</span>
+            <span class="text-sm text-surface-500">
+                Waiting for the host to select a trial
+            </span> 
+        </div>
+    {/if}
+</div>
 
