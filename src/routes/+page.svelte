a70ca2baac447480aec418b0ee8a471a2c5def65
Hugos68 <hugokorteapple@gmail.com>
2025-03-30 12:54:57 +0200

    remove route, use live

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,7 @@ import JoinLobby from "$lib/components/join-lobby.svelte";
 import Layout from "$lib/components/layout.svelte";
 import LeaveLobby from "$lib/components/leave-lobby.svelte";
 import SelectTrial from "$lib/components/select-trial.svelte";
+import TrialOverlay from "$lib/components/trial-overlay.svelte";
 import { useRealtimeRecord } from "$lib/hooks/use-realtime-record.svelte";
 import { supabase } from "$lib/supabase/client";
 import { error } from "@sveltejs/kit";
@@ -28,18 +29,24 @@ const lobby = $derived(
 		},
 	}),
 );
+
+let showTrialOverlay = $state(false);
 </script>
 
-<Layout>
-	<div class="grid gap-2">
-		{#if lobby.current}
-			<CopyLobbyId lobby={lobby.current} />
-			<SelectTrial lobby={lobby.current} trials={data.trials} user={data.user} />
-			<LeaveLobby lobby={lobby.current} user={data.user} />
-		{:else}
-			<JoinLobby />
-			<CreateLobby  />
-		{/if}
-	</div>	  
-</Layout>
+{#if lobby.current && showTrialOverlay}
+	<TrialOverlay lobby={lobby.current} onClose={() => (showTrialOverlay = false)} />
+{:else if !showTrialOverlay}
+	<Layout>
+		<div class="grid gap-2">
+			{#if lobby.current}
+				<CopyLobbyId lobby={lobby.current} />
+				<SelectTrial lobby={lobby.current} trials={data.trials} user={data.user} onOpen={() => (showTrialOverlay = true)} />
+				<LeaveLobby lobby={lobby.current} user={data.user} />
+			{:else}
+				<JoinLobby />
+				<CreateLobby  />
+			{/if}
+		</div>	  
+	</Layout>
+{/if}
 
